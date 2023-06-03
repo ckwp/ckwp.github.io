9b6f5e449d53bc3bc0f4b03dd64c6108c69a4296
ckwp <126389030+ckwp@users.noreply.github.com>
2023-06-03 17:46:10 +0900

    Add files via upload

---
+++ fes/quiz/script.js
@@ -0,0 +1,10 @@
+// 問題と正解のリスト
+const questions = [
+    { question: "問題1: 1 + 1は？", answer: "2" },
+    { question: "問題2: w ll ll ll", answer: "df" },
+];
+
+document.getElementById("submit-btn").addEventListener("click", function() {
+    // ボタンがクリックされたときの処理をここに書く
+    console.log("ボタンがクリックされました");
+});
